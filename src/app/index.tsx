f0a42391e2380389079db920aa905779ffefa7c7
James Edmonston <james@jamesedmonston.co.uk>
2025-08-26 22:04:09 +0100

    add logo to index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,45 +9,53 @@ import { View } from 'react-native';
 
 export default function Index() {
 	return (
-		<View className='w-full flex-1 justify-center self-center'>
-			<RandomDots count={20} />
+		<View className='flex-1 items-center'>
+			<Image
+				className='top-safe absolute left-1/2 h-16 w-16 -translate-x-1/2 overflow-hidden rounded-3xl'
+				source={require('@/assets/icon.png')}
+				contentFit='contain'
+			/>
 
-			<Stack gap={6}>
-				<View className='mx-auto w-[85vw]'>
-					<Stack direction='row' gap={4}>
-						<View className='flex-1 shadow-xl shadow-black/20'>
-							<Card>
-								<Aspect ratio={216 / 128}>
-									<Image source={require('@/assets/dog-1.jpg')} contentFit='cover' />
-								</Aspect>
-							</Card>
-						</View>
+			<View className='flex-1 justify-center'>
+				<RandomDots count={20} />
 
-						<View className='flex-1 shadow-xl shadow-black/20'>
-							<Card>
-								<Aspect ratio={216 / 128}>
-									<Image source={require('@/assets/dog-2.jpg')} contentFit='cover' />
-								</Aspect>
-							</Card>
-						</View>
-					</Stack>
-				</View>
+				<Stack gap={6}>
+					<View className='mx-auto w-[85vw]'>
+						<Stack direction='row' gap={4}>
+							<View className='flex-1 shadow-xl shadow-black/20'>
+								<Card>
+									<Aspect ratio={216 / 128}>
+										<Image source={require('@/assets/dog-1.jpg')} contentFit='cover' />
+									</Aspect>
+								</Card>
+							</View>
 
-				<View className='w-[70vw] justify-center self-center'>
-					<Stack gap={6}>
-						<Text size='3xl' weight='bold' align='center'>
-							Your dream dog is just{`\r\n`}a boop away
-						</Text>
+							<View className='flex-1 shadow-xl shadow-black/20'>
+								<Card>
+									<Aspect ratio={216 / 128}>
+										<Image source={require('@/assets/dog-2.jpg')} contentFit='cover' />
+									</Aspect>
+								</Card>
+							</View>
+						</Stack>
+					</View>
 
-						<Text size='xl' align='center' alpha='faded'>
-							Because every dog deserves a forever home, and every person deserves to boop the
-							snoot.
-						</Text>
+					<View className='w-[70vw] justify-center self-center'>
+						<Stack gap={6}>
+							<Text size='3xl' weight='bold' align='center'>
+								Your dream dog is just{`\r\n`}a boop away
+							</Text>
 
-						<Button href='/explore'>Start matching</Button>
-					</Stack>
-				</View>
-			</Stack>
+							<Text size='xl' align='center' alpha='faded'>
+								Because every dog deserves a forever home, and every person deserves to boop the
+								snoot.
+							</Text>
+
+							<Button href='/explore'>Start matching</Button>
+						</Stack>
+					</View>
+				</Stack>
+			</View>
 		</View>
 	);
 }
