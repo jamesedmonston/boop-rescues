9d150881e214e0eba3059fd02c25c31e55bdedb7
James Edmonston <james@jamesedmonston.co.uk>
2025-08-26 22:05:50 +0100

    vary dot sizes

--- src/components/ui/random-dots.tsx
+++ src/components/ui/random-dots.tsx
@@ -8,12 +8,14 @@ interface Props {
 export function RandomDots({ count = 20 }: Props) {
 	const colors = ['bg-primary', 'bg-secondary'];
 	const opacities = ['opacity-10', 'opacity-20', 'opacity-30', 'opacity-40', 'opacity-50'];
+	const sizes = ['h-2 w-2', 'h-3 w-3', 'h-4 w-4'];
 	const minDistance = 4;
 	const dots: {
 		left: number;
 		top: number;
 		color: string;
 		opacity: string;
+		size: string;
 	}[] = [];
 
 	for (let i = 0; i < count; i++) {
@@ -35,12 +37,14 @@ export function RandomDots({ count = 20 }: Props) {
 			if (validPosition) {
 				const color = colors[Math.floor(Math.random() * colors.length)];
 				const opacity = opacities[Math.floor(Math.random() * opacities.length)];
+				const size = sizes[Math.floor(Math.random() * sizes.length)];
 
 				dots.push({
 					left,
 					top,
 					color,
 					opacity,
+					size,
 				});
 
 				break;
@@ -51,7 +55,7 @@ export function RandomDots({ count = 20 }: Props) {
 	}
 
 	return dots.map((dot, i) => {
-		const className = classify(['absolute h-4 w-4 rounded-full', dot.color, dot.opacity]);
+		const className = classify(['absolute rounded-full', dot.size, dot.color, dot.opacity]);
 
 		return (
 			<View
